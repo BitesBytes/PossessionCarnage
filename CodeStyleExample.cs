79c298e66eaebb9f3e02e0ed8da9894ff5e382e9
BPA98 <bpaone.dev@gmail.com>
2023-06-03 15:48:54 +0200

    Test
    Test GitHub

---
+++ CodeStyleExample.cs
@@ -0,0 +1,50 @@
+//Questa classe è stata realizzata grazie al contributo di Codemonkey
+
+//Various requirements
+[RequireComponent(typeof(Rigidbody))]
+
+//Class
+public class CodeStyleExample
+{
+    //Contants: UpperCase SnakeCase
+    public const int CONSTANT_FIELD = 56;
+
+    //Porperties: PascalCase
+    public static CodeStyleExample Instance { get; private set; }
+
+    //Events: PascalCase
+    public event EventHandler[<OnSomethingHappenedEventArgs>] OnSomethingHappened;
+    //Other Event classes
+    public class OnSomethingHappenedEventArgs: EventArgs
+    {
+        public int randomNum;
+    }
+
+    //Serialized Fields: camelCase
+    [SerializeField] private int randomNum;
+
+    //Private Fields: camelCase
+    private float membervariable;
+
+    //Public Fields: PascalCase
+    public float TestPublicVariable;
+
+    //Function Names: PascalCase
+    private void Awake()
+    {
+        Instance = this;
+
+        DoSomething(10f);
+    }
+
+    //Function Params: camelCase
+    private void DoSomething(float time)
+    {
+        //Do something...
+        membervariable = time * Time.deltaTime;
+        if(membervariable > 0f)
+        {
+            //Do something else ...
+        }
+    }
+}
