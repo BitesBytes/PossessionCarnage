727a9e8a9716719054f579daeb6a75ceb2e493be
Graced <simone.bisato37@gmail.com>
2023-06-08 15:25:33 +0200

    Development
    all attack system

---
+++ Assets/_Main/Simone/Scripts/LongAttackArea.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LongAttackArea : MonoBehaviour
+{
+    private float lightattackdamage;
+    private float heavyattackdamage;
+
+
+    // HEALTH SCRIPT 
+}
---
+++ Assets/_Main/Simone/Scripts/SpecialAttackArea.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialAttackArea : MonoBehaviour
+{
+    private float specialattackdamage;
+}
