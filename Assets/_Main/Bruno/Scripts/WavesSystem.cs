552c08be1338fb45f6a2ee15080df962c575b765
BPA98 <bpaone.dev@gmail.com>
2023-06-22 11:10:32 +0200

    Development
    Add basic WaveSystem

---
+++ Assets/_Main/Bruno/Scripts/WavesSystem.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WavesSystem : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> enemies;
+    [SerializeField] private List<Transform> spawnPoints;
+
+    private float spawnInterval = 15f;
+    private float enemiesPerWave = 3;
+    private float currentWave = 0f;
+    private float timeBeetweenWaves = 15f;
+    private float nextSpawnTime = 15f;
+    private float nextWaveTime = 15f;
+
+    private float timerSincePlay; // tempo quando premi play
+    private float endGameTimer = 300; // 5 minuti
+
+
+
+    private void Start()
+    {
+        Begin();
+    }
+
+    private void Update()
+    {
+        timerSincePlay = Time.time;
+
+        if(timerSincePlay >= endGameTimer) // se il tempo supera 5 minuti hai vinto
+        {
+            Debug.Log("you survived");
+        }
+
+        if(timerSincePlay >= nextSpawnTime)
+        {
+            Begin();
+            nextSpawnTime = timerSincePlay + spawnInterval;
+        }
+    }
+
+    private void Begin()
+    {
+        currentWave++;
+        nextSpawnTime = timerSincePlay + timeBeetweenWaves;
+        nextWaveTime = timerSincePlay + nextWaveTime;
+
+        for(int i = 0; i < enemiesPerWave; i++)
+        {
+            Debug.Log("spawning next wave");
+            SpawnWave();
+        }
+
+        Debug.Log("current wave: " + currentWave);
+
+    }
+
+    private void SpawnWave()
+    {
+        GameObject ai = GetRandomAI();
+        ai = Instantiate(ai, spawnPoints[GetRandomIdx()].position, spawnPoints[GetRandomIdx()].rotation);
+    }
+
+    private GameObject GetRandomAI()
+    {
+        int idx = Random.Range(0, enemies.Count);
+        return enemies[idx];
+    }
+
+    private int GetRandomIdx()
+    {
+        return Random.Range(0, spawnPoints.Count);
+    }
+}
